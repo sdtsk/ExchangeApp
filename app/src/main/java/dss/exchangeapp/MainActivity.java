65e5845e89f10cf3d634c7b385479ce56b355e86
sdtsk <dotsenko_tsk@mail.ru>
2021-03-24 14:42:51 +0700

    add list of valutes

--- app/src/main/java/dss/exchangeapp/MainActivity.java
+++ app/src/main/java/dss/exchangeapp/MainActivity.java
@@ -1,14 +1,130 @@
 package dss.exchangeapp;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import dss.exchangeapp.CustomArrayAdapter;
+import dss.exchangeapp.ListItemClass;
+import dss.exchangeapp.R;
+
 
 public class MainActivity extends AppCompatActivity {
+    //    private Document doc;
+    private RequestQueue mQueue;
+    private Thread secThread;
+    private Runnable runnable;
+    private ListView listView;
+    private CustomArrayAdapter adapter;
+    private List<ListItemClass> arrayList;
+//    public String val_1;
+//    public String val_2;
+//    public String val_3;
+//    public String val_4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init()
+    {
+        listView = findViewById(R.id.listView);
+        mQueue = Volley.newRequestQueue(this);
+        arrayList = new ArrayList<>();
+        adapter = new CustomArrayAdapter(this,R.layout.list_item_1,arrayList,getLayoutInflater());
+        listView.setAdapter(adapter);
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                getWeb();
+            }
+        };
+        secThread = new Thread(runnable);
+        secThread.start();
+
+    }
+
+
+    public void getWeb() {
+
+        String url = "https://www.cbr-xml-daily.ru/daily_json.js";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONObject valuteObj = response.getJSONObject("Valute");
+                            Iterator x = valuteObj.keys();
+                            JSONArray listOfValute = new JSONArray();
+
+                            while (x.hasNext()) {
+                                String key = (String) x.next();
+                                listOfValute.put(valuteObj.get(key));
+                            }
+
+                            for (int i = 0; i < listOfValute.length(); i++) {
+                                JSONObject nameVal = listOfValute.getJSONObject(i);
+                                Iterator y = nameVal.keys();
+                                JSONArray mapOfValute = new JSONArray();
+
+                                while (x.hasNext()) {
+                                    String key = (String) x.next();
+                                    mapOfValute.put(nameVal.get(key));
+                                }
+
+                                String charCode = nameVal.getString("CharCode");
+                                String nominal = nameVal.getString("Nominal");
+                                String name = nameVal.getString("Name");
+                                String value = nameVal.getString("Value");
+
+                                ListItemClass items = new ListItemClass();
+                                items.setData_1(charCode);
+                                items.setData_2(nominal);
+                                items.setData_3(name);
+                                items.setData_4(value);
+                                arrayList.add(items);
+                            }
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    adapter.notifyDataSetChanged();
+                                }
+                            });
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mQueue.add(request);
     }
 }
